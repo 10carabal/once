d577d250110d086d78cb325fce3664ac6fa327ba
daniel.otero <daniel.otero@usc.edu.co>
2025-10-16 09:46:32 -0500

    feat: add authentication and layout components with basic templates

---
+++ src/app/pages/components/match/match.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match',
+  imports: [],
+  templateUrl: './match.component.html',
+  styleUrl: './match.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MatchComponent { }
---
+++ src/app/pages/components/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProfileComponent { }
