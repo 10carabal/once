bba2d103031ac6f637921fbf6a860ca549b4e17f
daniel.otero <daniel.otero@usc.edu.co>
2025-10-16 11:17:50 -0500

    feat: Rearrange navigation and footer components in the Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { NavBarComponent } from '../shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent, NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
