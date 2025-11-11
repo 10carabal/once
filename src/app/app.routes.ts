942de6d2e545a69ee97da69e6395f20af143ec20
daniel.otero <daniel.otero@usc.edu.co>
2025-11-11 14:51:05 -0500

    feat: Update navigation bar styles and structure; enhance hover effects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'match', loadComponent: () => import('./pages/components/match/match.component'), data: { title: 'match', showInNavBar: true } },
-  { path: 'training', loadComponent: () => import('./pages/components/training/training.component'), data: { title: 'training', showInNavBar: true } },
-  { path: 'history', loadComponent: () => import('./pages/components/history/history.component'), data: { title: 'history', showInNavBar: true } },
-  { path: 'profile', loadComponent: () => import('./pages/components/profile/profile.component'), data: { title: 'profile', showInNavBar: true } },
+  { path: 'match', loadComponent: () => import('./pages/components/match/match.component'), data: { title: 'Partido', showInNavBar: true } },
+  { path: 'training', loadComponent: () => import('./pages/components/training/training.component'), data: { title: 'Entrenamiento', showInNavBar: true } },
+  { path: 'history', loadComponent: () => import('./pages/components/history/history.component'), data: { title: 'Historial', showInNavBar: true } },
+  { path: 'profile', loadComponent: () => import('./pages/components/profile/profile.component'), data: { title: 'Perfil', showInNavBar: true } },
   { path: '**', redirectTo: 'home' }
 ];
