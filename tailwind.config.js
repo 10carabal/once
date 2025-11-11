ab8c8cf9744bb98f7274f271205fe806ec04b61f
daniel.otero <daniel.otero@usc.edu.co>
2025-11-11 14:51:45 -0500

    feat: Add CSS variables for theme colors in styles and update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
+    colors: {
+      primary: 'var(--primary-color)',
+      secondary: 'var(--secondary-color)',
+      accent: 'var(--accent-color)',
+      background: 'var(--background-color)',
+      text: 'var(--text-color)',
+    },
     extend: {},
   },
   plugins: [],
